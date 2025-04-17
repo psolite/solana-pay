1fb1bcae186d22b787ada7bb9750aa5af73c8c3e
psolite <diamond212x@gmail.com>
2025-04-17 16:51:42 +0100

    post function added

--- src/app/qr/route.tsx
+++ src/app/qr/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { clusterApiUrl, Connection, Keypair, PublicKey, TransactionMessage, VersionedTransaction } from '@solana/web3.js';
+import { clusterApiUrl, Connection, PublicKey, TransactionMessage, VersionedTransaction } from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
 import { createSplTransferIx } from '@/component/transfer';
 
