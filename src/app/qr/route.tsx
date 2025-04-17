d5543b155b200d1734cecba2b01570f23169bcd0
psolite <diamond212x@gmail.com>
2025-04-17 19:46:56 +0100

    checking

--- src/app/qr/route.tsx
+++ src/app/qr/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import { PublicKey } from '@solana/web3.js';
 import { createSplTransferIx } from '@/component/transfer';
 
 export async function GET(request: Request) {
